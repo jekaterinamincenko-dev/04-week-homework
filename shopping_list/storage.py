2045491be4066445e27363f1628ca8aea7e1c34b
Jekaterina <yeka@inbox.lv>
2026-03-08 02:19:59 +0200

    add: renewed shopping list and added price list

--- shopping_list/storage.py
+++ shopping_list/storage.py
@@ -1,17 +1,45 @@
 import json
 import os
 
-FILE_NAME = "shopping.json"
+SHOP_FILE = "shopping.json"
+PRICE_FILE = "prices.json"
+
+# IEPIRKUMU SARAKSTS
 
 def load_list():
     """Nolasa shopping.json. Ja fails neeksistē, atgriež tukšu sarakstu."""
-    if not os.path.exists(FILE_NAME):
+    if not os.path.exists(SHOP_FILE):
         return []
 
-    with open(FILE_NAME, "r", encoding="utf-8") as f:
+    with open(SHOP_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
 
 def save_list(items):
     """Saglabā iepirkumu sarakstu JSON failā."""
-    with open(FILE_NAME, "w", encoding="utf-8") as f:
+    with open(SHOP_FILE, "w", encoding="utf-8") as f:
         json.dump(items, f, indent=2, ensure_ascii=False)
+
+# CENU DATUBAZE
+
+def load_prices():
+    if not os.path.exists(PRICE_FILE):
+        return {}
+
+    with open(PRICE_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def save_prices(prices):
+    with open(PRICE_FILE, "w", encoding="utf-8") as f:
+        json.dump(prices, f, indent=2, ensure_ascii=False)
+
+
+def get_price(name):
+    prices = load_prices()
+    return prices.get(name)
+
+
+def set_price(name, price):
+    prices = load_prices()
+    prices[name] = price
+    save_prices(prices)
